4b3d2366db17dbfcaf833d470005c99787fe2af0
Tahir Hussain Mansuri <tahirmansuri@Tahirs-MacBook-Pro.local>
2022-08-04 10:40:04 +0530

    MODIFIED:[Tahir] PLayer Input Sybmol as X or O for Playing Game

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,8 +1,14 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
     private static char[] board = new char[10];
+
+    static private char player1;
+
+    static Scanner sc = new Scanner(System.in);
 
     /***
      * UC-1 - This method is Assigning space to Board Array
@@ -13,10 +19,25 @@ public class TicTacToe {
         }
     }
 
+    /**
+     * UC-2 - Taking Playing symbol from User as X OR O only
+     */
+    static void playerInput(){
+        System.out.println("Player 1 Please Select the Symbol (X/O) :");
+        player1 = sc.next().charAt(0);
+        if( player1 == 'X' || player1 == 'O') {
+            System.out.println("Player 1 Symbol : " +player1);
+        } else {
+            System.out.println("Invalid Symbol. Please Enter the X or O only.");
+            playerInput();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Simulation");
 
         startGame();
 
+        playerInput();
     }
 }
